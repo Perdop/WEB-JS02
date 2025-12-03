ebdc497d3c1a64d959c94dacc84faac00fb2c8e4
Pedro <operdop@proton.me>
2025-12-03 09:59:12 -0300

    feat Consultar Postagens por Id
    - Método findById(id: number) na Classe PostagemService
    - Método findById(id: number) na Classe PostagemController

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,6 +1,13 @@
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from './../services/postagem.service';
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 
 @Controller('/postagens')
 export class PostagemController {
@@ -10,5 +17,11 @@ export class PostagemController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findById(id);
   }
 }
